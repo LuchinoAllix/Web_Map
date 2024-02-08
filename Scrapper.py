3dbcd2d5154803c79cfe1e8505f37834a40bbd8b
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2024-02-08 13:06:31 -0500

    good for 1rst test

--- Scrapper.py
+++ Scrapper.py
@@ -2,6 +2,8 @@ import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import colorama
+import os
+import shutil
 
 # init the colorama module
 colorama.init()
@@ -9,6 +11,8 @@ GREEN = colorama.Fore.GREEN
 GRAY = colorama.Fore.LIGHTBLACK_EX
 RESET = colorama.Fore.RESET
 YELLOW = colorama.Fore.YELLOW
+
+pages = {}
 
 # Source :
 # https://thepythoncode.com/article/extract-all-website-links-python
@@ -35,21 +39,20 @@ def get_links(url,choice="all"):
 		parsed_href = urlparse(href)
 		# remove URL GET parameters, URL fragments, etc.
 		href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
-		if filter(href):
+		if filter(href,url):
 			if domain_name not in href:
 				# external link
 				if href not in external_urls:
-					print(f"{GRAY}[!] External link: {href}{RESET}")
+					#print(f"{GRAY}[!] External link: {href}{RESET}")
 					external_urls.add(href)
 			elif href not in internal_urls:
-				print(f"{GREEN}[*] Internal link: {href}{RESET}")
+				#print(f"{GREEN}[*] Internal link: {href}{RESET}")
 				internal_urls.add(href)
 			urls.add(href)
 
 	if choice=="all":
 		return url
 	elif choice=="intern":
-		print(internal_urls)
 		return internal_urls
 	elif choice=="extern":
 		return external_urls
@@ -57,20 +60,32 @@ def get_links(url,choice="all"):
 		raise ValueError("Choice must be \"all\", \"intern\" or \"extern\"")
 	
 
-	
-def filter(url):
-	filters = ["Category","Wikipedia","ISBN","Special","File"]
+def filter(href,url):
+	if href==url:
+		return False
+	if href in pages :
+		return False
+	filters = ["Category","Wikipedia","ISBN","Special","File","index"]
 	res = True
 	for filter in filters :
-		if filter in url :
+		if filter in href :
 			res = False
 			break
 	return res
 
+def get_title(url):
+	segments = url.split('/')
+	return segments[-1].replace("_"," ")
 
+def add_to_pages(url,urls):
+	text = ""
+	for link in urls:
+		text+="[["+str(get_title(link))+"]]\n"
+	global pages
+	pages[url] = text
+	
 # number of urls visited so far will be stored here
 total_urls_visited = 0
-
 
 def crawl(url, max_urls):
 	"""
@@ -80,24 +95,22 @@ def crawl(url, max_urls):
 		max_urls (int): number of max urls to crawl, default is 30.
 	"""
 	global total_urls_visited
-	global pages
 	total_urls_visited += 1
 	print(f"{YELLOW}[*] Crawling: {url}{RESET}")
 	links = get_links(url,"intern")
+	add_to_pages(url,links)
 	for link in links:
 		if total_urls_visited > max_urls:
 			break
 		crawl(link, max_urls=max_urls)
+	
 
 if __name__ == "__main__":
-	max_urls=30
-	urls = get_links("https://en.wikipedia.org/wiki/Second_War_of_Scottish_Independence","intern")
-	
-	furls = filter(urls)
-	for i in furls :
-		print(i)
-	print(len(urls))
-	print(len(furls))
-
-	#crawl("https://en.wikipedia.org/wiki/Second_War_of_Scottish_Independence",max_urls)
+	max_urls=100
+	crawl("https://en.wikipedia.org/wiki/Second_War_of_Scottish_Independence",max_urls)
+	shutil.rmtree("Logs")
+	os.makedirs("Logs")
+	for key in pages :
+		with open("logs/"+get_title(key)+".md","w") as file:
+			file.write(pages[key])
 
