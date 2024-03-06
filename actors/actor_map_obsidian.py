1d74b2db4f133c5febce322c786d4e77c1fe06b7
LuchinoAllix <60980620+LuchinoAllix@users.noreply.github.com>
2024-03-06 11:50:26 -0500

    commentaires

--- actors/actor_map_obsidian.py
+++ actors/actor_map_obsidian.py
@@ -11,14 +11,14 @@ with open(actors,encoding='utf8') as csv_file :
 	dico["Chris Pratt"] = ['Omar Sy']
 	dico["Omar Sy"] = ['Chris Pratt']
 	for row in csv_reader:
-		if len(row)>1 :
+		if len(row)>1 : # Parce que la première ligne comprend "\ufeff" donc traiter manuellement
 			pass
 		else :
 			names = []
 			name =''
 			inLink = False
 			i = 0
-			while i < len(row[0]) :
+			while i < len(row[0]) : #rajout des noms par ligne(film)
 				c = row[0][i]
 				if c == '(' :
 					inLink = True
@@ -31,7 +31,7 @@ with open(actors,encoding='utf8') as csv_file :
 				elif not inLink :
 					name += c
 				i+=1
-			for name in names :
+			for name in names : # rajout des noms dans le dico
 				if name not in dico :
 					dico[name] = []
 				for other_name in names :
@@ -39,9 +39,9 @@ with open(actors,encoding='utf8') as csv_file :
 						if name not in dico[name] :
 							dico[name].append(other_name)
 
-del dico['~Not Notable~']
+del dico['~Not Notable~'] #on retire les éléments non voulus
 
-for name in dico :
+for name in dico : # on écrit le dico dans les fichiers (séparé par lettre parce que obsidian a du mal sinon)
 	loc = os.path.join(rep,"files",name[0],name+".md")
 	with open(loc,"w",encoding="UTF8") as file:
 		content = ""
